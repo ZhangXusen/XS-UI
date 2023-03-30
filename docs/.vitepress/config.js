0078bc9a2019f14626ff555b6f561b176ed27a3c
zxs <461694365@qq.com>
2023-03-31 00:59:26 +0800

    修改了部署代码

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 	// 		href: "/public/element-plus-logo.svg",
 	// 	},
 	// ],
-	base: "/Elements-plus-plus/docs/",
+	base: "/Elements-plus-plus/",
 	themeConfig: {
 		siteTitle: false,
 		logo: "/element-plus-logo.svg",
